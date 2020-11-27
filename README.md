bb616fb6fa8240c3528c768379a0cf06ac7e5fbe
Martin Zlámal <mrtnzlml@gmail.com>
2020-11-27 09:35:38 -0600

    Deprecate '@adeira/test-utils'
    I was thinking for a while what to do with `@adeira/test-utils` package. There are only two functions (`evaluateGraphQLResolver` and `generateTestsFromFixtures`) the GraphQL resolver evaluation seems to be pretty useless/outdated. On the other hand the fixtures test generator is quite popular and helpful.
    
    I'd like to propose a deprecation of https://www.npmjs.com/package/@adeira/test-utils and I'd like to create a new NPM package instead which is specializing only on the fixtures testing.
    
    adeira-source-id: 214ec1ccb0e91b58d003fb761dd74ac880dad64b

---
+++ README.md
@@ -0,0 +1,25 @@
+```text
+yarn add @adeira/fixtures-tester
+```
+
+## `generateTestsFromFixtures`
+
+This function loads all the fixtures (their file content) from your folder and it snapshots the result of your callback operation. This is handy when you have several example files, you want to perform some operation on top of them and snapshot the results.
+
+```js
+import { generateTestsFromFixtures } from '@adeira/fixtures-tester';
+
+function operation(input) {
+  return doSomethingWithTheInput(input);
+}
+
+generateTestsFromFixtures(`${__dirname}/__fixtures__`, operation);
+```
+
+There are two additional modes which allow you to control the fixtures behavior more precisely. There are based on the fixture name:
+
+- `simpleExample.graphql` - fixture will snapshot as expected
+- `simpleExample.only.graphql` - only these snapshots will run (for debug only)
+- `simpleExample.error.graphql` - it's expected that fixture will throw an error (expected to fail tests)
+
+The error file convention is required to prevent unintentional snapshots of exceptions.
---
+++ src/generateTestsFromFixtures.js
@@ -0,0 +1,97 @@
+// @flow
+
+import os from 'os';
+import { isObject } from '@adeira/js';
+
+const FIXTURE_TAG = Symbol.for('FIXTURE_TAG');
+
+/**
+ * Extend Jest with a custom snapshot serializer to provide additional context
+ * and reduce the amount of escaping that occurs. This serializer is heavily
+ * inspired by Relay test cases.
+ */
+expect.addSnapshotSerializer({
+  print(serializerValue) {
+    return Object.keys(serializerValue)
+      .map((key) => {
+        const value = serializerValue[key];
+        const inspectedValue = isObject(value) ? JSON.stringify(value, null, 2) : value;
+        return `~~~~~~~~~~ ${key.toUpperCase()} ~~~~~~~~~~\n${inspectedValue}`;
+      })
+      .join(os.EOL);
+  },
+  test(value) {
+    return value && value[FIXTURE_TAG] === true;
+  },
+});
+
+type OperationOutput = any | Promise<any>;
+
+/**
+ * Generates a set of jest snapshot tests that compare the output of the
+ * provided `operation` to each of the matching files in the `fixturesPath`.
+ */
+export default function generateTestsFromFixtures( // eslint-disable-line jest/no-export
+  fixturesPath: string,
+  operation: (input: string) => OperationOutput,
+  snapshotName?: string,
+): void {
+  const fs = require('fs');
+  const path = require('path');
+
+  let fixtures = fs.readdirSync(fixturesPath);
+
+  test(`has fixtures in ${fixturesPath}`, () => {
+    expect(fixtures.length > 0).toBe(true);
+  });
+
+  const shouldSkip = (file) => /\.only\.\w+$/.test(file);
+  const onlyFixtures = fixtures.filter(shouldSkip);
+  if (onlyFixtures.length) {
+    // $FlowFixMe[prop-missing]: we need to update our Jest type definitions (TODO)
+    test.skip.each(fixtures.filter((name) => !shouldSkip(name)))(
+      'matches expected output: %s',
+      () => {},
+    );
+    fixtures = onlyFixtures;
+  }
+
+  test.each(fixtures)('matches expected output: %s', async (file) => {
+    const input = fs.readFileSync(path.join(fixturesPath, file), 'utf8');
+    const output = await getOutputForFixture(input, operation, file);
+    if (snapshotName != null) {
+      expect({
+        // $FlowIssue[invalid-computed-prop]: https://github.com/facebook/flow/issues/3258
+        [FIXTURE_TAG]: true,
+        input: input,
+        output: output,
+      }).toMatchSnapshot(snapshotName);
+    } else {
+      expect({
+        // $FlowIssue[invalid-computed-prop]: https://github.com/facebook/flow/issues/3258
+        [FIXTURE_TAG]: true,
+        input: input,
+        output: output,
+      }).toMatchSnapshot();
+    }
+  });
+}
+
+async function getOutputForFixture(
+  input: string,
+  operation: (input: string) => OperationOutput,
+  file: string,
+): Promise<any> {
+  const shouldThrow = /\.error\.\w+$/.test(file);
+  if (shouldThrow) {
+    let result;
+    try {
+      result = await operation(input);
+    } catch (error) {
+      return `THROWN EXCEPTION:\n\n${error.toString()}`;
+    }
+    throw new Error(`Expected test file '${file}' to throw, but it passed:\n${result}`);
+  } else {
+    return operation(input);
+  }
+}
